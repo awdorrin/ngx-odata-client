2fe61b942b8092e6670a551c5ce2d80a9b076f39
Dorrington, Albert <albert.dorrington@lmco.com>
2025-06-25 13:26:03 -0400

    Update for Angular 18

--- projects/ngx-odata-client/src/lib/ngx-odata-client.module.ts
+++ projects/ngx-odata-client/src/lib/ngx-odata-client.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ODataServiceFactory } from './odata-service-factory';
 import { ODataConfiguration, KeyConfigs } from './odata-configuration';
 
-@NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-  ],
-  exports: [
-  ],
-  providers: [
-    ODataServiceFactory,
-    ODataConfiguration
-  ]
-})
+@NgModule({ declarations: [],
+    exports: [], imports: [CommonModule], providers: [
+        ODataServiceFactory,
+        ODataConfiguration,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class NgxODataClientModule { }
